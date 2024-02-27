1332afa8fc6b88657fd173a66295ebb0a6854449
Imsety <imsety@imsety.com>
2024-02-26 22:53:58 -0500

    Add Umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,19 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { theme } from 'krado-react';
 import { ThemeUIProvider } from 'theme-ui';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeUIProvider theme={{ ...theme, ...setyTheme }}>
+      {process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="lazyOnload"
+          />
+        )}
       <Component {...pageProps} />
     </ThemeUIProvider>
   );
