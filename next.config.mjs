cceafe189128ea080fba073eacefc13b3baae0e6
Imsety <imsety@imsety.com>
2024-03-18 00:09:11 -0400

    Add Sanity CDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['assets.tina.io']
+    domains: ['assets.tina.io', 'cdn.sanity.io']
   },
   experimental: {
     scrollRestoration: true
