5e942f72c617a58eea7357af1a6a0743c61a14f3
Imsety <imsety@imsety.com>
2024-03-18 21:04:27 -0400

    Info page

--- pages/info.tsx
+++ pages/info.tsx
@@ -43,7 +43,7 @@ export default function Faq() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <FaqHero name=' Emir and team' />
+      <FaqHero />
       <Container sx={{ marginBottom: 6 }}>
         <ReelSection
           isVideoActive={isVideoActive}
