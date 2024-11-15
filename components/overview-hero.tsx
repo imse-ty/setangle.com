7d65bbab0c6951e1a57ea780c1cc300305a1724c
Imsety <imsety@imsety.com>
2024-11-15 12:50:28 -0500

    Update slots

--- components/overview-hero.tsx
+++ components/overview-hero.tsx
@@ -56,7 +56,7 @@ export default function OverviewHero() {
         >
           <AvailabilityIndicator />
           <Text variant="body.footnote" sx={{ color: 'accent.light' }}>
-            Limited availability. 1 slot left for October.
+            Limited availability. 2 slots left for November.
           </Text>
         </Flex>
         <Flex sx={{ flexDirection: ['column', 'row'], gap: 3 }}>
