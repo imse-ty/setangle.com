f40e8730a1c7d4101bc16f89c9ac07be90dc96f4
Imsety <imsety@imsety.com>
2025-08-11 17:20:34 -0400

    Add logos to `LogoCarousel`

---
+++ src/lib/LogoCarousel.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Chickfila from './logos/Chickfila.svelte';
+	import Ford from './logos/Ford.svelte';
+	import Google from './logos/Google.svelte';
+	import KaiserPermanente from './logos/KaiserPermanente.svelte';
+	import Maxon from './logos/Maxon.svelte';
+	import Vmware from './logos/Vmware.svelte';
+</script>
+
+<div>
+	<Chickfila />
+	<Ford />
+	<Google />
+	<KaiserPermanente />
+	<Maxon />
+	<Vmware />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Header from '$lib/Header.svelte';
+	import LogoCarousel from '$lib/LogoCarousel.svelte';
 	import HomeHero from '../components/HomeHero.svelte';
 </script>
 
 <Header />
 <HomeHero />
+<LogoCarousel />
