148e251f4a801252c31441af8ccf8d63bbf61c1e
Imsety <imsety@imsety.com>
2025-08-11 18:27:47 -0400

    Move logos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import ContactSection from '$lib/ContactSection.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import Header from '$lib/Header.svelte';
-	import LogoCarousel from '$lib/LogoCarousel.svelte';
+	import LogoCarousel from '../components/LogoCarousel.svelte';
 	import AboutSection from '../components/AboutSection.svelte';
 	import HomeHero from '../components/HomeHero.svelte';
 	import ProjectsSection from '../components/ProjectsSection.svelte';
@@ -10,7 +10,7 @@
 
 <Header />
 <HomeHero />
-<!-- <LogoCarousel /> -->
+<LogoCarousel />
 <ProjectsSection />
 <AboutSection />
 <ContactSection />
