23d3dd3758943a6e4c9d1294093b394beb311a42
Imsety <imsety@imsety.com>
2025-08-12 11:43:59 -0400

    Update container auto margin

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -2,4 +2,4 @@
 	let { children } = $props();
 </script>
 
-<div class="container px-16 xl:px-32.5">{@render children()}</div>
+<div class="container mx-auto px-16 xl:px-32.5">{@render children()}</div>
