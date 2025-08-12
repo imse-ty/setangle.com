710dde4667cf5ef5e3a870b821bba88ba8a28ce8
Imsety <imsety@imsety.com>
2025-08-12 12:28:38 -0400

    Style about section

--- src/components/AboutSection.svelte
+++ src/components/AboutSection.svelte
@@ -1,7 +1,14 @@
-<div>
-	<h2>About</h2>
-	<p>
-		We craft bold motion design and creative development that blends art and tech to help brands
-		connect and inspire.
-	</p>
-</div>
+<script>
+	import Container from '$lib/Container.svelte';
+</script>
+
+<Container>
+	<div class="flex gap-8">
+		<h2 class="font-mono uppercase">About</h2>
+		<p class="text-4xl">
+			We craft bold motion design and creative development <span class="text-set-gray"
+				>that blends art and tech to help brands connect and inspire.</span
+			>
+		</p>
+	</div>
+</Container>
