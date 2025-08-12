dffecb0a8e3b6d56cfb12544d377e41a913ce8d2
Imsety <imsety@imsety.com>
2025-08-12 11:20:21 -0400

    Style `HomeHero`

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,7 +2,7 @@
 	import NavItem from './NavItem.svelte';
 </script>
 
-<div class="flex items-center gap-4">
+<div class="flex items-center gap-4 py-6">
 	<div class="font-display text-3xl font-bold">ANGLE</div>
 	<div class="h-px w-full bg-set-gray"></div>
 	<ul class="flex gap-5">
