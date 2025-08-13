f12be6df10a8e829b1e33f225a63c8532dadb38e
Imsety <imsety@imsety.com>
2025-08-13 11:26:56 -0400

    Make `ProjectCard` clickable

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -2,9 +2,11 @@
 	let { title, subtitle } = $props();
 </script>
 
-<div class="flex h-128 border-1 border-set-gray p-8">
-	<div class="mt-auto">
-		<h3 class="text-3xl">{title}</h3>
-		<p class="text-set-gray">{subtitle}</p>
+<a href="/work">
+	<div class="flex h-128 border-1 border-set-gray p-8">
+		<div class="mt-auto">
+			<h3 class="text-3xl">{title}</h3>
+			<p class="text-set-gray">{subtitle}</p>
+		</div>
 	</div>
-</div>
+</a>
