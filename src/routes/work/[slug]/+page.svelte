c603e61cd4fc69f0623837dacf26335615dee50b
Imsety <imsety@imsety.com>
2025-09-02 00:17:32 -0400

    Make HTML blockout use real data

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -3,29 +3,20 @@
 	import { PortableText } from '@portabletext/svelte';
 	import { urlFor } from '$lib/sanity';
 	import ContactSection from '$lib/ContactSection.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import WorkVideo from '../../../components/WorkVideo.svelte';
+	import Container from '$lib/Container.svelte';
+	import WorkHero from '../../../components/WorkHero.svelte';
 	export let data;
 	const { doc } = data;
 </script>
 
 <svelte:head><title>{doc.title}</title></svelte:head>
 
-<div class="flex flex-col gap-20 bg-set-black pb-24 font-body text-set-white">
-	<Header />
-	<h1 class="font-display text-5xl font-medium tracking-tight lg:text-8xl xl:text-9xl">
-		{doc.title}
-	</h1>
-
-	<p>{doc.subtitle}</p>
-
-	<a href="/#work"><button class="btn btn-outline">Go back</button></a>
-
-	{#if doc.cover?.asset}
-		<img
-			class="max-w-1/2"
-			src={urlFor(doc.cover).width(1600).url()}
-			alt={doc.cover?.alt || doc.title}
-		/>
-	{/if}
-
+<div class="flex flex-col gap-20 bg-set-black font-body text-set-white">
+	<Container><Header /></Container>
+	<WorkVideo />
+	<WorkHero title={doc.title} summary={doc.subtitle} info={doc.info} stats={doc.stats} />
 	<ContactSection />
+	<Footer />
 </div>
