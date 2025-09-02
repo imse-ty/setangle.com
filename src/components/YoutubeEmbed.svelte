8c9201defff089f9f7211c572c269002f1ca623e
Imsety <imsety@imsety.com>
2025-09-02 00:19:30 -0400

    Port v1.0 video embed components

---
+++ src/components/YoutubeEmbed.svelte
@@ -0,0 +1,20 @@
+<script>
+	import getVideoId from 'get-video-id';
+	export let url;
+
+	$: youtubeId = url ? getVideoId(url)?.id : null;
+	$: youtubeUrl = youtubeId ? `https://www.youtube.com/embed/${youtubeId}` : null;
+</script>
+
+{#if youtubeUrl}
+	<figure class="relative my-4 aspect-video overflow-hidden lg:-mx-[25%] xl:-mx-[50%]">
+		<iframe
+			src={youtubeUrl}
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			class="absolute inset-0 h-full w-full"
+			allowfullscreen
+		></iframe>
+	</figure>
+{/if}
