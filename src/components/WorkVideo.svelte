ca91f9a54026b9ba1563c98a378ac2147733c32b
Imsety <imsety@imsety.com>
2025-09-02 00:19:42 -0400

    Use real video in case study

---
+++ src/components/WorkVideo.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Container from '$lib/Container.svelte';
+	import VimeoEmbed from './VimeoEmbed.svelte';
+</script>
+
+<!-- Video/Image Banner Box, think this can go at the end maybe -->
+<Container>
+	<VimeoEmbed url="https://vimeo.com/168456009?fl=pl&fe=sh" />
+</Container>
