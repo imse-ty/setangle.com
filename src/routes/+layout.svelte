af7bd84dcbc8743fd4a73b5aac79332d83131462
Imsety <imsety@imsety.com>
2025-09-20 15:07:31 -0400

    Add preview mode banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
 	import '../app.css';
-
+	import favicon from '$lib/assets/favicon.svg';
 	import { isPreviewing, VisualEditing } from '@sanity/visual-editing/svelte';
 	import { page } from '$app/stores';
 	import LiveMode from '../components/LiveMode.svelte';
+	import Container from '$lib/Container.svelte';
 </script>
 
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
 {#if $isPreviewing}
-	<a href={`/preview/disable?redirect=${$page.url.pathname}`} class="preview-toggle">
-		<span>Preview Enabled</span>
-		<span>Disable Preview</span>
-	</a>
+	<div class="sticky top-0 z-50 bg-set-purple py-1.5 text-set-white">
+		<Container>
+			<div class="flex w-full items-center justify-between">
+				<span class="font-mono text-sm font-medium"> You are currently in preview mode </span>
+				<a href={`/preview/disable?redirect=${$page.url.pathname}`}>
+					<button class="btn cursor-pointer font-mono font-normal uppercase btn-outline btn-sm">
+						Disable preview mode
+					</button>
+				</a>
+			</div>
+		</Container>
+	</div>
 {/if}
 
 <slot />
@@ -19,47 +32,3 @@
 	<VisualEditing />
 	<LiveMode />
 {/if}
-
-<style>
-	.preview-toggle {
-		backdrop-filter: blur(12px);
-		border-radius: 0.25rem;
-		bottom: 1rem;
-		box-shadow:
-			0 10px 15px -3px rgba(0, 0, 0, 0.1),
-			0 4px 6px -2px rgba(0, 0, 0, 0.05);
-		color: #1f2937;
-		display: block;
-		font-size: 0.75rem;
-		font-weight: 500;
-		line-height: 1rem;
-		padding-bottom: 0.5rem;
-		padding-left: 0.75rem;
-		padding-right: 0.75rem;
-		padding-top: 0.5rem;
-		position: fixed;
-		right: 1rem;
-		text-align: center;
-		text-decoration: none;
-		z-index: 50;
-	}
-
-	.preview-toggle:hover {
-		background-color: #ef4444;
-		color: #ffffff;
-	}
-
-	.preview-toggle span:first-child {
-		display: block;
-	}
-	.preview-toggle:hover span:first-child {
-		display: none;
-	}
-
-	.preview-toggle span:last-child {
-		display: none;
-	}
-	.preview-toggle:hover span:last-child {
-		display: block;
-	}
-</style>
