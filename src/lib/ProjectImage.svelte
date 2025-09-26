bf08e37e36055c8342e6398d166e97abf10ffef6
Imsety <imsety@imsety.com>
2025-09-26 10:49:32 -0400

    Add `ProjectImage`

---
+++ src/lib/ProjectImage.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { urlFor } from './sanity/image';
+
+	let { portableText } = $props();
+
+	console.log(portableText);
+</script>
+
+<img
+	src={urlFor(portableText.value.asset).width(1200).url()}
+	alt={portableText.value.alt}
+	loading="lazy"
+/>
