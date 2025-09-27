83888389e7b10e7f1cdb51fecadaa857a6f0920a
Imsety <imsety@imsety.com>
2025-09-26 20:46:31 -0400

    Adjust header and footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,25 +2,27 @@
 	import Container from './Container.svelte';
 </script>
 
-<Container>
-	<footer class="flex flex-col gap-4 py-6 md:flex-row md:items-center">
-		<p class="font-mono uppercase">Copyright © 2025 Set Angle. All rights reserved.</p>
-		<div class="h-px flex-1 bg-set-gray"></div>
-		<div class="flex gap-4">
-			<a
-				class="btn cursor-pointer p-0 font-mono font-normal uppercase btn-ghost"
-				href="https://www.instagram.com/set_angle"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<button class="uppercase">Instagram</button></a
-			>
-			<a
-				class="btn cursor-pointer p-0 font-mono font-normal uppercase btn-ghost"
-				href="https://www.linkedin.com/company/setangle"
-				target="_blank"
-				rel="noopener noreferrer"><button class="uppercase">LinkedIn</button></a
-			>
+<footer class="bg-black">
+	<Container>
+		<div class="flex flex-col gap-4 py-6 md:flex-row md:items-center">
+			<p class="font-mono uppercase">Copyright © 2025 Set Angle. All rights reserved.</p>
+			<div class="h-px flex-1 bg-set-gray"></div>
+			<div class="flex gap-4">
+				<a
+					class="btn cursor-pointer p-0 font-mono font-normal uppercase btn-ghost"
+					href="https://www.instagram.com/set_angle"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<button class="uppercase">Instagram</button></a
+				>
+				<a
+					class="btn cursor-pointer p-0 font-mono font-normal uppercase btn-ghost"
+					href="https://www.linkedin.com/company/setangle"
+					target="_blank"
+					rel="noopener noreferrer"><button class="uppercase">LinkedIn</button></a
+				>
+			</div>
 		</div>
-	</footer>
-</Container>
+	</Container>
+</footer>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,18 +3,16 @@
 	import NavItem from './NavItem.svelte';
 </script>
 
-<header class="fixed top-0 z-50 w-full backdrop-blur-xs">
-	<Container>
-		<div class="flex items-center gap-4 py-6">
-			<a href="/"><div class="font-display text-3xl font-bold">ANGLE</div></a>
-			<div class="h-px w-full bg-set-gray"></div>
-			<nav>
-				<ul class="hidden gap-5 md:flex">
-					<NavItem href="/#work">Work</NavItem>
-					<NavItem href="/#about">About</NavItem>
-					<NavItem href="#contact">Contact</NavItem>
-				</ul>
-			</nav>
-		</div>
-	</Container>
+<header class="fixed top-0 z-50 w-full px-8 backdrop-blur-xs md:px-16 xl:px-32.5">
+	<div class="flex items-center gap-4 py-6">
+		<a href="/"><div class="font-display text-3xl font-bold">ANGLE</div></a>
+		<div class="h-px w-full bg-set-gray"></div>
+		<nav>
+			<ul class="hidden gap-5 md:flex">
+				<NavItem href="/#work">Work</NavItem>
+				<NavItem href="/#about">About</NavItem>
+				<NavItem href="#contact">Contact</NavItem>
+			</ul>
+		</nav>
+	</div>
 </header>
