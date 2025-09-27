22e30da6ba4f6737a9b73a45c4fb0265dab28cbb
Imsety <imsety@imsety.com>
2025-09-26 21:44:24 -0400

    Re-order stuff

--- src/lib/ContactSection.svelte
+++ src/lib/ContactSection.svelte
@@ -23,19 +23,21 @@
 				<span class="text-set-gray">Lets</span> talk
 			</h2>
 			<div class="flex flex-col gap-8 text-3xl tracking-widest text-neutral-100">
-				<p class="font-medium">ATL {hour - 12}:{min}:{sec} {dayOrNight} (GMT-4)</p>
+				<p class="font-bold">ATL {hour - 12}:{min}:{sec} {dayOrNight} (GMT-4)</p>
+
+				<div class="flex flex-col gap-2">
+					<a href="mailto:greetings@setangle.com">greetings@setangle.com</a>
+					<a href="tel:+17706095007">+1 (770) 609-5007</a>
+				</div>
+
 				<a
 					href="https://maps.app.goo.gl/Av6uMgwiCBgUkPyn8"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="font-mono text-neutral-300 uppercase"
+					class="font-mono text-set-gray uppercase"
 				>
 					<p>104 Church St <br />Decatur, GA 30030<br /> USA</p>
 				</a>
-
-				<a href="tel:+17706095007" class="font-mono">+1 (770) 609-5007</a>
-
-				<a href="mailto:greetings@setangle.com" class="font-mono">greetings@setangle.com</a>
 			</div>
 		</div></Container
 	>
