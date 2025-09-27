581791a4545df819d1d09cad76966bfca1fd6416
Imsety <imsety@imsety.com>
2025-09-26 21:29:00 -0400

    No more form

--- src/lib/ContactSection.svelte
+++ src/lib/ContactSection.svelte
@@ -8,28 +8,21 @@
 			<h2 class="font-display text-9xl font-medium uppercase">
 				<span class="text-set-gray">Lets</span> talk
 			</h2>
-			<div class="flex flex-col gap-8">
-				<p class="text-2xl text-set-gray">
-					We help brands shine by creating soulful animated visuals for marketing campaigns and
-					events.
-				</p>
-				<div class="flex h-32 flex-col md:flex-row">
-					<a
-						class="btn h-full flex-1 font-mono font-normal btn-outline"
-						href="https://cal.com/team/setangle/discuss"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<button class="uppercase">Book a call</button></a
-					>
-					<a
-						class="btn h-full flex-1 font-mono font-normal btn-outline"
-						href="mailto:greetings@setangle.com"
-					>
-						<button class="uppercase">Send an email</button></a
-					>
-				</div>
+			<div class="flex flex-col gap-8 text-3xl tracking-widest text-neutral-100">
+				<p class="font-bold">ATL 9:06 PM</p>
+				<a
+					href="https://maps.app.goo.gl/Av6uMgwiCBgUkPyn8"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-neutral-300"
+				>
+					<p>104 Church St <br />Decatur, GA 30030<br /> USA</p>
+				</a>
+
+				<a href="tel:+17706095007">+1 (770) 609-5007</a>
+
+				<a href="mailto:greetings@setangle.com">greetings@setangle.com</a>
 			</div>
-		</div>
-	</Container>
+		</div></Container
+	>
 </section>
