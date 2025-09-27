22b7583da2299dead4d75f09910f3b72c486bb7f
Imsety <imsety@imsety.com>
2025-09-26 20:46:21 -0400

    Add hover animation to cards and make it look cooler

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -3,12 +3,16 @@
 </script>
 
 <a {href}>
-	<div class="relative flex h-128 p-8">
-		<div class="z-20 mt-auto">
-			<h3 class="text-3xl">{title}</h3>
+	<div class="relative flex h-128 p-8 transition duration-300 hover:scale-97">
+		<div class="z-20 mt-auto flex flex-col gap-4">
+			<h3 class="font-display text-4xl font-medium text-neutral-200 uppercase">{title}</h3>
 			<p class="text-set-gray">{subtitle}</p>
 		</div>
 		<div class="absolute top-0 left-0 z-10 h-full w-full"></div>
+
+		<div
+			class="absolute top-0 left-0 z-10 h-full w-full bg-set-black opacity-0 transition duration-300 hover:opacity-30"
+		></div>
 
 		<img
 			class="absolute top-0 left-0 h-full w-full rounded-sm object-cover"
