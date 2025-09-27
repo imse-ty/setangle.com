2583bd4999263688cb444c4b36080e098871d78d
Imsety <imsety@imsety.com>
2025-09-26 21:41:24 -0400

    Make time actually work

--- src/lib/ContactSection.svelte
+++ src/lib/ContactSection.svelte
@@ -1,5 +1,19 @@
 <script>
 	import Container from './Container.svelte';
+	import { onMount } from 'svelte';
+
+	let date = new Date();
+	$: hour = date.getHours();
+	$: min = date.getMinutes();
+	$: sec = date.getSeconds();
+	let dayOrNight = 'AM';
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			date = new Date();
+			dayOrNight = hour >= 12 ? 'PM' : 'AM';
+		}, 1000);
+	});
 </script>
 
 <section id="contact" class="h-screen">
@@ -9,19 +23,19 @@
 				<span class="text-set-gray">Lets</span> talk
 			</h2>
 			<div class="flex flex-col gap-8 text-3xl tracking-widest text-neutral-100">
-				<p class="font-bold">ATL 9:06 PM</p>
+				<p class="font-medium">ATL {hour - 12}:{min}:{sec} {dayOrNight} (GMT-4)</p>
 				<a
 					href="https://maps.app.goo.gl/Av6uMgwiCBgUkPyn8"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="text-neutral-300"
+					class="font-mono text-neutral-300 uppercase"
 				>
 					<p>104 Church St <br />Decatur, GA 30030<br /> USA</p>
 				</a>
 
-				<a href="tel:+17706095007">+1 (770) 609-5007</a>
+				<a href="tel:+17706095007" class="font-mono">+1 (770) 609-5007</a>
 
-				<a href="mailto:greetings@setangle.com">greetings@setangle.com</a>
+				<a href="mailto:greetings@setangle.com" class="font-mono">greetings@setangle.com</a>
 			</div>
 		</div></Container
 	>
