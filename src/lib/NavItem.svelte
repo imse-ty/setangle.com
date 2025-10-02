b413c5389b4a7e191be5e336e52342abaf7c9f02
Imsety <imsety@imsety.com>
2025-10-02 09:49:55 -0400

    Add hover animation to NavItem

--- src/lib/NavItem.svelte
+++ src/lib/NavItem.svelte
@@ -2,6 +2,11 @@
 	let { children, href, isActive = false } = $props();
 </script>
 
-<li class="font-mono uppercase">
-	<a {href} aria-current={isActive} class="aria-current:font-bold">{@render children()}</a>
+<li class="font-mono uppercase hover:text-set-gray">
+	<a
+		{href}
+		aria-current={isActive}
+		class="border-b-1 border-transparent transition-colors duration-300 hover:border-inherit aria-current:border-inherit aria-current:font-bold"
+		>{@render children()}</a
+	>
 </li>
