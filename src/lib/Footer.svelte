90ef8b5a63c0512e1becd2298bf4985d4909928b
Imsety <imsety@imsety.com>
2026-08-23 12:40:13 -0400

    Update phone number link in Footer component

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -38,7 +38,7 @@
 						<ContactItem label="Email" href="mailto:greetings@setangle.com"
 							>greetings@setangle.com</ContactItem
 						>
-						<ContactItem label="Phone" href="tel:+17706095007">+1 ‪(943) 300-4585‬</ContactItem>
+						<ContactItem label="Phone" href="tel:+19433004585">+1 ‪(943) 300-4585‬</ContactItem>
 						<ContactItem
 							label="Office"
 							href="https://maps.app.goo.gl/ZeJSzEodHtZppjoQA"
